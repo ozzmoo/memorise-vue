9b664493deca19c72c0cf620e6db6c61fd50dd46
ozzmo <mestervalera@gmail.com>
2019-07-15 18:58:46 +0500

    Логика пустого поля

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,54 +1,62 @@
 <template>
   <div id="game">
-    <h3 class="rus"> {{rus}} </h3>
-    <input v-model="eng" type="text" name="" id="eng-input">
-    <input type="button" value="check" class="game-button" @click="checkWord">
-    <input type="button" value="next" class="game-button" @click="getWord">
+    <h3 class="rus">{{rus}}</h3>
+    <input v-model="eng" type="text" name id="eng-input" />
+    <input type="button" value="check" class="game-button" @click="checkWord" />
+    <input type="button" value="next" class="game-button" @click="getWord" />
   </div>
 </template>
 
 <script>
-
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "game",
   data() {
     return {
-      eng:'',
-      rus: 'click next',
-      username: 'ozzmo',
-      unit: 'unit1',
-      checkeng: ''
+      eng: "",
+      rus: "click next",
+      username: "ozzmo",
+      unit: "unit1",
+      checkeng: ""
     };
   },
   methods: {
-    getWord: function(){
-      const input = document.getElementById('eng-input').style.backgroundColor = "white"
-      axios.post('http://localhost:3000/game', {
-        username: this.username,
-        unit: this.unit
-      })
-      .then((response) =>{
-        const words = response.data
-        const engword = words.eng
-        const rusword = words.rus
-        this.rus = rusword
-        this.checkeng = engword
-      })
+    getWord: function() {
+      const input = (document.getElementById(
+        "eng-input"
+      ).style.backgroundColor = "white");
+      this.eng = '' //Очищаем поле ввода
+      axios
+        .post("http://localhost:3000/game", {
+          username: this.username,
+          unit: this.unit
+        })
+        .then(response => {
+          const words = response.data;
+          const engword = words.eng;
+          const rusword = words.rus;
+          this.rus = rusword;
+          this.checkeng = engword;
+        });
     },
-    checkWord: function(){
-      const input = document.getElementById('eng-input')
-      if (this.checkeng == this.eng){
-          input.style.backgroundColor = "#00ff2a8f"
-          console.log("Верно")
-        } else {
-          input.style.backgroundColor = "#ff4b4b94"
-          console.log("Ошибка")
-        }
+    checkWord: function() {
+      const input = document.getElementById("eng-input");
+      if (this.eng == "") {
+        input.style.backgroundColor = "#ffd667d5"; //В желтый?
+        console.log("Пустое поле");
+        return;
+      }
+      if (this.checkeng == this.eng) {
+        input.style.backgroundColor = "#00ff2a8f"; //Окраска в зеленый
+        console.log("Верно");
+      } else {
+        input.style.backgroundColor = "#ff4b4b94"; //В красный
+        console.log("Ошибка");
+      }
     }
   }
-}
+};
 </script>
 
 
@@ -73,10 +81,10 @@ export default {
 
   color: rgb(95, 95, 95);
   font-weight: light;
-  font-size: 25px;  
+  font-size: 25px;
   text-align: center;
 }
-.game-button{
+.game-button {
   cursor: pointer;
   margin: 10px auto;
   outline: none;
@@ -87,20 +95,19 @@ export default {
   padding: 2px 10px;
   color: rgb(95, 95, 95);
   font-weight: light;
-  font-size: 15px; 
+  font-size: 15px;
 }
 
-.game-button:active{
-  transition: .1s;
+.game-button:active {
+  transition: 0.1s;
   border: 2px solid rgb(145, 117, 117);
 }
 
-.rus{
+.rus {
   text-align: center;
   margin: 10px auto;
   width: 100%;
   color: rgb(95, 95, 95);
-  font-size: 30px; 
+  font-size: 30px;
 }
-
 </style>
